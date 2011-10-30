e4a4069f78db59bc0b8cfd0a6e270b7c7236860c
lishujie@ciandt.com <lishujie@ciandt.com@03c74eda-a174-d310-12cc-0d7cddd50488>
2011-10-30 13:17:15 +0000

    initial

---
+++ trunk/public_html/sites/default/files/languages/zh-hans_fe4e32cdcae2c83d3850f09c1f9f0203_0.js
@@ -0,0 +1,1 @@
+Drupal.locale = { 'strings': { "Drag to re-order": "拖放重新排序", "Changes made in this table will not be saved until the form is submitted.": "在此表格中的修改只有在整个表单提交后才会被保存。", "The changes to these blocks will not be saved until the \x3cem\x3eSave blocks\x3c/em\x3e button is clicked.": "这些区块的变更不会被储存起来，除非您按下\x3cem\x3e储存区块\x3c/em\x3e的按钮。" } };
